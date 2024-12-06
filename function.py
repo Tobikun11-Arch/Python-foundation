ea97f6238b52e2720416f18432bcddb97311c208
Joenel <joenelsevellejo831@gmail.com>
2024-12-06 21:40:54 +0800

    first commit

---
+++ function.py
@@ -0,0 +1,32 @@
+name = "joenel"
+age = 20
+
+def myAge():
+    print(f"my age is {age}")
+
+def myPersonal():
+    print("My name is " + name)
+
+
+# myPersonal()
+# myAge()
+
+# x = -2341
+
+# print(type(x))
+
+# num1 = 10
+# num = float(num1)
+# print(num)
+
+# x = int(10)
+# print(x)
+
+# paragraph = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+
+# print(paragraph)
+
+print(name.replace("j", "h"))
---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello world")
+a = 10
+
+x, y, z = "this is x", 10, 3.10
+
+print(x,y,z)
+
+print(type(a))
+
+if type(a) == type("a"):
+    print("True")
+
+else:
+    print("false")
+
+number = [10, 20, 40]
+f,g,h = number
+print(f,g,h)
